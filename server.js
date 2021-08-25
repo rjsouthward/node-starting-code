97c6d0e6b9ef201edddbe07b446c00a1f4463c02
rjsouthward <rysouth32@gmail.com>
2021-08-24 20:36:33 -0500

    NodeJS Boilerplate

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+let port = process.env.PORT
+if (port == null||port ==""){
+  port = 3000;
+}
+
+
+app.listen(port, function(){
+  console.log("Listening on a Port")
+});
+
+app.use(express.static(__dirname+"/public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
